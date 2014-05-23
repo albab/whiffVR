344c8e4665652c684c7ad3a5a893a08cf9d6fc0b
Alex <ababich@asu.edu>
2014-05-22 19:14:46 -0700

    Added test environment for new servo prototype

---
+++ Whiff/Assets/Uniduino/Scripts/Trigger2.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using Uniduino;
+ 
+public class Trigger2 : MonoBehaviour {
+ 
+    public Arduino arduino;
+	
+	public int pin = 9;
+	
+ 
+    void Start( )
+	{
+        arduino = Arduino.global;
+		arduino.Setup(ConfigurePins);
+    }
+	
+	void ConfigurePins() {
+		arduino.pinMode(9, PinMode.SERVO);
+	}
+		
+
+	void OnTriggerEnter(Collider other) {
+	    arduino.analogWrite(pin, 0);
+    }
+	
+	void OnTriggerExit(Collider other) {
+		arduino.analogWrite(pin, 60);
+	}
+
+}
+
+
+
